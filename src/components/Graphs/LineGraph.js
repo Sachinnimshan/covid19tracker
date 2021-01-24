0a8e7cf7728a61dc6b22b21ba39b8a38d77c7e0e
Sachin Nimshan <sachinnimshan@gmail.com>
2021-01-24 21:58:02 +0530

    latest

--- src/components/Graphs/LineGraph.js
+++ src/components/Graphs/LineGraph.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './LineGraph.css';
+import {Line} from 'react-chartjs2';
 
 function LineGraph() {
+
+    const [data, setdata] = useState({});
+
+    useEffect(()=>{
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+
+    },[]);
+
     return (
         <div className='line-graph-container'>
             <div className='line-graph-title'><strong>WorldWide New Cases</strong></div>
